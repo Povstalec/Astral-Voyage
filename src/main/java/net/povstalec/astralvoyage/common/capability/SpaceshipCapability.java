366b0aee316e1e6df60cd9aee142c7b1fceec477
mistersecret312 <sergiigamer7@gmail.com>
2024-06-09 14:30:55 +0300

    Confusion

--- src/main/java/net/povstalec/astralvoyage/common/capability/SpaceshipCapability.java
+++ src/main/java/net/povstalec/astralvoyage/common/capability/SpaceshipCapability.java
@@ -34,7 +34,7 @@ public class SpaceshipCapability implements INBTSerializable<CompoundTag>
 
     private static final String RENDER_OBJECTS = "render_objects";
 
-	private Vector3f galacticPosition = new Vector3f(11700, 11900, 19900);
+	private Vector3f galacticPosition = new Vector3f(10, 11, 19);
     private Vector3f solarPosition = new Vector3f(0, 0, 0);
 	private Vector3f oldGalacticPosition = new Vector3f(0, 0, 0);
     private Vector3f oldSolarPosition = new Vector3f(0, 0, 0);
